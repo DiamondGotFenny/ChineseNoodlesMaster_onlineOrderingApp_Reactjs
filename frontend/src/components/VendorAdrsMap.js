b3b6c5c4b55ec437520c212232abcb14ebea3c64
diamondcn923 <diamondcn923@gmail.com>
2021-02-01 19:08:12 +0800

    update map

--- frontend/src/components/VendorAdrsMap.js
+++ frontend/src/components/VendorAdrsMap.js
@@ -3,10 +3,12 @@ import  Modal  from 'react-bootstrap/Modal';
 import GoogleMapReact from 'google-map-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { REACT_APP_GOOGLE_MAP_APIKEY } from 'services/TemEnvService';
 
 const Marker = () => <div><FontAwesomeIcon icon={faMapMarkerAlt} size="lg" color="red"/></div>;
 const VendorAdrsMap = (props) => {
     const {show,onHide,coordinate}=props;
+    const apiKey=REACT_APP_GOOGLE_MAP_APIKEY;
   const center={
     lat:coordinate.lat,
     lng: coordinate.lng
@@ -19,7 +21,7 @@ const VendorAdrsMap = (props) => {
         </Modal.Header >
         <Modal.Body  style={{ height: '50vh', width: '100%' }} >
             <GoogleMapReact
-                bootstrapURLKeys={{ key: "AIzaSyCAoYWLlH902m5l7BzMHHBI1lHLsZ_Fu4w" }}
+                bootstrapURLKeys={{ key: apiKey }}
                 defaultCenter={center}
                 defaultZoom={15}
             >
